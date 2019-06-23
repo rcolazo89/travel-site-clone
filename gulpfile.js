b78020137e2227762d7ccd781470b822f761eac0
Reuben <rcolazo89@gmail.com>
2019-06-23 14:19:23 -0700

    finished browsersync #16

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ var autoprefixer = require('autoprefixer');
 var cssvars = require('postcss-simple-vars');
 var nested = require('postcss-nested');
 var cssimport = require('postcss-import');
+var browserSync = require('browser-sync').create();
 
 
 
@@ -27,11 +28,23 @@ gulp.task('styles',function(){
 
 gulp.task('watch', function(){
 
+    browserSync.init({
+        server: {
+            baseDir: "app"
+        }
+    });
+
     watch('./app/index.html', function(){
-        gulp.start('html');
+        browserSync.reload();
     });
 
     watch('./app/assets/styles/**/*.css', function(){
-        gulp.start('styles');
+        gulp.start('cssInject');
     });
+});
+
+gulp.task('cssInject', ['styles'],function(){
+    return gulp
+    .src('./app/temp/styles/styles.css') 
+    .pipe(browserSync.stream());
 });
