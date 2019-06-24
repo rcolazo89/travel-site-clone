8b49448ab2e44545adedd0e00e142339edd5c730
Reuben <rcolazo89@gmail.com>
2019-06-23 18:02:31 -0700

    finished mobile first lesson #19

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -4,11 +4,12 @@ var autoprefixer = require('autoprefixer');
 var cssvars = require('postcss-simple-vars');
 var nested = require('postcss-nested');
 var cssimport = require('postcss-import');
+var mixins = require('postcss-mixins');
 
 gulp.task('styles',function(){
     return gulp
     .src('./app/assets/styles/styles.css')
-    .pipe(postcss([cssimport, cssvars, nested, autoprefixer]))
+    .pipe(postcss([cssimport, mixins, cssvars, nested, autoprefixer]))
     .on('error', function(errorInfo){
         console.log(errorInfo.toString());
         this.emit('end');
